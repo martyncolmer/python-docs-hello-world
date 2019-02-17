3914039afb3b1a10a8f9c5736ac3b24a5cd42a5e
Martyn Colmer <martyc1974@hotmail.com>
2019-02-17 02:18:20 +0000

    Update message

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='martapp',
+    version='0.1dev',
+    packages=['martapp',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
