51027ad0609be5f2c89c67a01993302648c59428
Martyn Colmer <martyc1974@hotmail.com>
2019-02-17 02:26:07 +0000

    added setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='martapp',
-    version='0.1dev',
+    version='0.1',
     packages=['martapp',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
 )
